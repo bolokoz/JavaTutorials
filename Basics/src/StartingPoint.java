f1167b5318281e52c6813601a0d4e79c6f1f6a14
Yuri F Becker <yurifbeck@gmail.com>
2013-11-20 23:13:16 -0300

    Teste 1

---
+++ Basics/src/StartingPoint.java
@@ -0,0 +1,7 @@
+public class StartingPoint {
+	public static void main(String yuri[]) {
+		int myFavNumber = 8;
+		String words = "AEOUAHEOAUHEOA";
+		System.out.print("Hello my fav number is " + myFavNumber + " " + words);
+	}
+}
